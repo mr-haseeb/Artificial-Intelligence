fd8ae8ade8c297830f222ff1b52056be2fc47192
Mr Haseeb <bolahaseeb@gmail.com>
2020-03-23 23:51:02 +0500

    added installtation file

--- docs/conf.py
+++ docs/conf.py
@@ -88,6 +88,9 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 #---sphinx-themes-----
+import os
+os.system('pip3 install rtcat_sphinx_theme')
+
 html_theme = 'rtcat_sphinx_theme'
 import rtcat_sphinx_theme
 html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
